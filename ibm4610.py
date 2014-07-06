c87acb903bbcf969f83a59d5264a58f559a2f3cb
Falcon Darkstar Momot <falcon.momot@leviathansecurity.com>
2014-07-06 03:09:30 -0600

    Make it work.  Add the encoded shadytel logo.

--- ibm4610.py
+++ ibm4610.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python2
 
+# example use: ibm4610.print_slip(printer, 220016, 310416376402461, 89014103233764024616, "Subscriber C")
+
 #library for printing to an IBM 4610 in emulation mode
 
 import random
+import serial
+from shadylogo import shadylogo
+
+def get_printer(dev="/dev/ttyS0", baud=19200, timeout=3.0):
+    return serial.Serial(dev, baudrate=baud, timeout=timeout)
+
+def reset(printer):
+    printer.write("\x10\x05\x40")
 
 def set_codepage(printer, page):
     # 0 CP437 USA
@@ -28,7 +38,7 @@ def set_barcode_human_readable_text(printer, param):
 
 def print_code39_barcode(printer, data):
     #data is constrained to [:digits:] in ASCII
-    printer.write("\x1D\x6B\x04{}\x00".format(data))
+    printer.write("\x1D\x6B\x04{0}\x00".format(data))
 
 def print_pdf417(printer, data):
     #limit 1000 characters
@@ -37,9 +47,16 @@ def print_pdf417(printer, data):
 def cut_paper(printer):
     printer.write("\x0C")
 
+def print_logo(printer, logo, x, y, density="\x00"):
+    printer.write("\x1B\x2A{0}{1}{2}{3}\x0A".format(density, chr(x/8), chr(y/8), logo))
+    # The image!  It is bits in a line, opposite bit order (LSB is leftmost pixel), for lines.  Line widths are multiples of 8.
+    # Then, you can print as many columns like that as you want.  But, alas, only so many characters in an image are supported (see manual).
+
 def print_slip(printer, ext, imsi, iccid, name):
     set_codepage(printer, 1)
+    print_logo(printer, shadylogo, 256, 184)
     set_charsize(printer, 4, 4)
+    printer.write("\n")
     printer.write("{0}".format(ext))
     set_charsize(printer, 1, 1)
     printer.write("\n\n")
@@ -53,6 +70,6 @@ def print_slip(printer, ext, imsi, iccid, name):
     printer.write(name)
     set_charsize(printer, 1, 1)
     printer.write("\n\n")
-    set_barcode_dimensions(printer, 5)
+    set_barcode_dimensions(printer, 50)
     print_code39_barcode(printer, ext)
     cut_paper(printer)
